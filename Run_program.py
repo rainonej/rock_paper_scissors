54b2775015a05cfe6126a4343e234f665f591951
Jordan <rainonej@gmail.com>
2019-03-10 14:17:45 -0400

    Update Run_program.py

--- Run_program.py
+++ Run_program.py
@@ -7,6 +7,7 @@ Created on Sat Mar  9 00:31:15 2019
 
 """import the list of players (and later also games and AIs), and enter it/them into the interface."""
 
+#We need the classes to know what we're importing
 import classes
 from classes import *
 
@@ -18,12 +19,7 @@ with open('players.pkl', 'rb') as input:
 import user_interface
 from user_interface import *
 
-#import AI
-#from AI import *
 
-#RPS = RPSgame('Rock, paper, scissors', ['ROCK', 'PAPER', 'SCISSORS'], [('ROCK', 'SCISSORS'), ('SCISSORS', 'PAPER'), ('PAPER', 'ROCK')] )
-
-#write into the shell interface(RPS, t)
 interface(RPS,t, player)
 
 
