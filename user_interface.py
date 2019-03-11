b98481c5067f40016246dfad8a3d9b63ba793395
Jordan <rainonej@gmail.com>
2019-03-10 23:43:32 -0400

    Made the interface look and run better

--- user_interface.py
+++ user_interface.py
@@ -17,42 +17,60 @@ def interface(game, AI, list_of_players):
 		player = list_of_players[-1]
 		del list_of_players[-1]
 		print('Welcome back ', player.name)
-	else:
+	else: 
 		#select the player from a list
 
-		print("select your file from the list or type in a name you don't see")
+		#Create list of player names for the input function
+		list_of_player_names = []
 		for i in range(0,len(list_of_players)):
-			print("[", i, "]", list_of_players[i].name)
-		#print("[", len(list_of_players)+1, "] New Player")
+			list_of_player_names.append(list_of_players[i].name)
 
-		ans = new_input()
-		if (int(ans) in range(0, len(list_of_players))):
-			player = list_of_players[int(ans)]
-			del list_of_players[int(ans)]
+		#select your save file
+		print('Select your file from the list or select NEW PLAYER.')
+		ans = collect_input(list_of_player_names + ['NEW PLAYER'])
+
+		if ans in range(0, len(list_of_player_names)):
+			player = list_of_players[ans]
+			del list_of_players[ans]
+
 		else:
-			player = Player(ans)
+			print('Enter your name:')
+
+			#Make sure they pick a valid name
+			choosing = True
+			while (choosing == True):
+				player_name = new_input()
+				player_name = player_name.upper()
+				if (player_name in list_of_player_names + ['NEW PLAYER', '']):
+					print("You can't pick that name, try again.")
+				else:
+					player = Player(player_name)
+					choosing = False
+
 
 	print("Great for you to join us ", player.name, "! Let's get started")
 
 	# Explains how the game is played 
 	print(game.rules)
 	
-
 	#theis is the actual game part
-
 
 	playing = True #setting up a condition to exit the loop
 	while(playing == True):
 
-		print('Your options are: ', game.elements)
-		print("Type 'q' to quit")
-		print('Your choice is:')
-		player_choice = new_input()
-		player_choice = player_choice.upper() #makes it the same case
+		print("Type 'quit' to quit")
+		temp = game.elements + ['QUIT']
+		player_choice = collect_input(temp)
+		player_choice = temp[player_choice]
 
-		if (player_choice != 'Q'):
 
-			#See who won
+		#If the player decided to quit the game
+		if (player_choice == 'QUIT'):
+			playing = False
+		
+		#If they made a valid choice, then we see who won.
+		else:
+			#player_choice = game.elements[player_choice]
 			AI_choice = gamePlay(game, player, player_choice, AI)
 
 			#Resolution message
@@ -62,10 +80,33 @@ def interface(game, AI, list_of_players):
 				print('Boo! ', AI_choice , ' beats ' ,  player_choice)
 			else:
 				print('its a tie')
-		else:
-			playing = False
 
 
 	list_of_players.append(player)
 	print('your record is', player.wlt)
+
+
+def collect_input(options):
+	"options should be given in all caps."
+	#list off the options
+	#num_options = list(enumerate(options))
+	numbers = []
+	for i in range(0,len(options)):
+		print("[", i, "]", options[i])
+		numbers.append(str(i))
+
+	#collect the answer
+	choosing = True
+	while (choosing == True):
+		print("you select:")
+		answer = new_input()
+		answer = answer.upper()
+		if (answer in options):
+			return options.index(answer)
+			choosing = False
+		elif(answer in numbers):
+			return int(answer)
+			choosing = False
+		else:
+			print('not a valid option')
 
