c264fd60aa8423df7f5b18dd708c386dee6e2714
Jordan <rainonej@gmail.com>
2019-03-14 02:15:25 -0400

    created the first test AI based on human data

--- AI.py
+++ AI.py
@@ -2,3 +2,36 @@ import classes
 from classes import *
 
 Default_AI = AI()
+Default_AI.game = RPS
+
+
+#Here's the first not completely random 'AI'. It's really a test AI since it should come from recorded human data
+from types import MethodType
+
+PizzaExpert = AI()
+PizzaExpert.game = RPS
+PizzaExpert.count = 6
+
+with open("data.txt", "r") as f:
+    datum = f.readlines()
+
+def translate(letter):
+	if (letter == 'p'):
+		return 'PAPER'
+	elif (letter == 'x'):
+		return 'SCISSORS'
+	elif (letter == 's'):
+		return 'ROCK'
+
+def eat_pizza(self):
+	while ((datum[self.count][0] != 'p') and  (datum[self.count][0] != 'x') and (datum[self.count][0] != 's')):
+		self.count += 1
+
+	output = translate(datum[self.count][0])
+	self.count += 1
+	return output
+
+PizzaExpert.choice = MethodType(eat_pizza, PizzaExpert)
+
+
+
---
+++ Fitness_test.py
@@ -0,0 +1,4 @@
+"""This is the place where we create a "fitness test" to test an AI"""
+
+"""Take 2 AI's and run them against each other"""
+
