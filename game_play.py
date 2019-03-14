e47061858d854457f08291993bdb7122244db71e
Jordan <rainonej@gmail.com>
2019-03-14 02:14:35 -0400

    Simplified a lot of the code

--- game_play.py
+++ game_play.py
@@ -6,36 +6,43 @@ def gamePlay(game, player, player_choice, AI):
 	"A function which takes in a game, a player, and a player input and determines who wins and updates the 'score cards' "
 
 	#Where the future AT will go
-	AI_choice = AI.choice(game, player)
+	AI_choice = AI.choice()
 
 
 	#where we determins who won
-	outcome = 'TIE'
+	outcome = 2
 	if ((player_choice, AI_choice) in game.winners):
-		outcome = 'player_wins'
+		outcome = 0 #Player Wins!
 	elif ((AI_choice, player_choice) in game.winners):
-		outcome = 'AI_wins'
+		outcome = 1 #AI wins
 	else:
-		outcome = 'TIE'
+		outcome = 2 #It's a TIE
+
+	#where ths 'score cards' get updated (but only if the player want's it to be updated)
+	if (player.record == True):
+
+		player.wlt[outcome] += 1
+		player.streak.append(outcome)
+		player.history.append((player_choice, AI_choice))
+
+	if (AI.record == True):
+
+		outcome = switch_outcome(outcome)
+		AI.wlt[outcome] += 1
+		AI.streak.append(outcome)
+		AI.history.append((AI_choice, player_choice))
 
 
-	#where ths 'score cards' get updated
-	if (outcome == 'player_wins'):
-		player.wlt[0] += 1
-		player.streak.append(0)
-	elif (outcome == 'AI_wins'):
-		player.wlt[1] += 1
-		player.streak.append(1)
-	else:
-		player.wlt[2] += 1
-		player.streak.append(2)
-
-
-	#Updating the records
-	player.history.append((player_choice, AI_choice))
 	#this should probably be a list pairs of numbers, to make the AI's life easier in the future.
 
 
 	#returns the choice
 	return AI_choice
 
+def switch_outcome(n):
+	if (n == 0):
+		return 1
+	elif (n == 1):
+		return 0
+	elif:
+		return 2
